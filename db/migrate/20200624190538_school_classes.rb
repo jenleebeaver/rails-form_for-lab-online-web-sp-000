34267afcf4a6e6b2bd37018b1862eb4f51278cbf
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-24 19:06:26 +0000

    Automatically backed up by Learn

--- db/migrate/20200624190538_school_classes.rb
+++ db/migrate/20200624190538_school_classes.rb
@@ -2,7 +2,7 @@ class SchoolClasses < ActiveRecord::Migration[5.0]
   def change
     create_table :school_classes do |t|
       t.string :title
-      t.string :last_name
+      t.integer :last_name
 
       t.timestamps null: false
   end
