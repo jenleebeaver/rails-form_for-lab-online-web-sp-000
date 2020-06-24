ee2427023f6c54ace902f98e4980cef25de7fb60
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-24 18:37:53 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
 
   def update
     set_student!
-    @school_class.update(school_class_params)
+    @student.update(school_class_params)
     redirect_to school_class_path(@school_class)
   end
 
