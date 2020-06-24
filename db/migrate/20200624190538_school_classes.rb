c4b534b5603b8107d9d5ac289f7baaf6a9b8f663
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-24 19:06:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200624190538_school_classes.rb
+++ db/migrate/20200624190538_school_classes.rb
@@ -1,4 +1,9 @@
 class SchoolClasses < ActiveRecord::Migration[5.0]
   def change
+    create_table :school_classes do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
   end
 end
